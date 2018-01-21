58c2d0d836cce0ffcfe8288b588b20181d0ac8cb
Jozue06 <jozue06@gmail.com>
2018-01-20 17:33:46 -0800

    fixed console logs. fixed final question, ready for deploy on master

--- js/app.js
+++ js/app.js
@@ -4,13 +4,13 @@ var userPoints = 0;
 //////GLOBAL VARIABLE THAT WILL STAY OUT OF THE FUNCTIONS
 
 
-console.log('Josh\'s about me guessing game');
+//console.log('Josh\'s about me guessing game');
 
 alert('Welcome to my guessing game!');
 
 var user = prompt('What is your name?');
 alert('Hi ' + user + '! I am so glad you want to play my game. Let\'s see how much you can find out about me, ok?');
-console.log('User\'s name is ' + user);
+//console.log('User\'s name is ' + user);
 
 
 
@@ -33,7 +33,7 @@ function questionOne() {
     do {
       var userAnswer = prompt(gameArray[0][i]).toLowerCase();
       userAnswerArray.push(userAnswer);
-      console.log(userAnswer);
+      //console.log(userAnswer);
       if (userAnswer === 'yes' || userAnswer === 'no') {
         yesOrNo = 1;
       }
@@ -42,15 +42,16 @@ function questionOne() {
     if (userAnswer === (gameArray[1][i])) {
       alert(correctAnswer[i]);
       userPoints++; //if answer is correct, displays message and gives point
-      console.log(''+ user + ' has ' + userPoints + ' point(s).'); //logs user points to debug in console if needed
+      //console.log(''+ user + ' has ' + userPoints + ' point(s).'); //logs user points to debug in console if needed
     } else {
       alert(wrongAnswer[i]);
     }//if answer is incorrect, displays message and moves on without point
-    console.log('' + user + '\'s answer to this question is '+ userAnswer); //logs the answer input to the question
+    // console.log('' + user + '\'s answer to this question is '+ userAnswer); //logs the answer input to the question
   }
 }
 questionOne();
-console.log('end of multidimensional array questions');
+//console.log('end of multidimensional array questions');
+
 
 function questionSix() {
   var numOfGuess = 0;
@@ -58,48 +59,49 @@ function questionSix() {
   while (numOfGuess < 3){
     var guess = prompt('Guess my Favorite number! (You have 3 chances)');
 
-    console.log('Your guess ' + guess);
+    //console.log('Your guess ' + guess);
     if (guess === favNum) {
       alert('Awesome 6 is my favorite number!');
-      console.log('Awesome 6 is my favorite number!');
+      //console.log('Awesome 6 is my favorite number!');
       userPoints++;
       break;
     }
     if (guess > favNum) {
       alert('Too high');
-      console.log('' + guess + ' is too high');  
+      //console.log('' + guess + ' is too high');  
     }
-    if (guess < favNum) {
+    else if (guess < favNum) {
       alert('Too low');
-      console.log('' + guess + ' is too low');
+      //console.log('' + guess + ' is too low');
     }
     numOfGuess++;
   }
 }
 questionSix();
 
+
 function questionSeven() {
   var randNum = Math.floor(Math.random() * Math.floor(10));
   var numOfGuess2 = 0;
-  console.log(randNum);
+  //console.log(randNum);
 
-  while (numOfGuess2 < 3){
-    var guessRand = prompt('Guess a random number between 0-10! (You have 3 chances)');
+  while (numOfGuess2 < 5){
+    var guessRand = prompt('Guess a random number between 0-10! (You have 5 chances)');
 
-    console.log('Your guess ' + guessRand);
-    if (guessRand === randNum) {
+    //console.log('Your guess ' + guessRand);
+    if (guessRand == randNum) {
       alert('Awesome you guessed the number!');
-      console.log('you guessed the random number!');
+      //console.log('you guessed the random number!');
       userPoints++;
       break;
     }
     if (guessRand > randNum) {
       alert('Too high');
-      console.log('' + guessRand + ' is too high');  
+      //console.log('' + guessRand + ' is too high');
     }
     if (guessRand < randNum) {
       alert('Too low');
-      console.log('' + guessRand + ' is too low');
+      //console.log('' + guessRand + ' is too low');
     }
     numOfGuess2++;
   }
@@ -107,17 +109,19 @@ function questionSeven() {
 questionSeven();
 
 
+
+
 function questionEight() {
   var numOfGuess3 = 0;
 
   while (numOfGuess3 < 5){
     var guessFood = prompt('Guess one of my favorite foods. (You have 5 chances)');
-    console.log('Your guess ' + guessFood);
+    //console.log('Your guess ' + guessFood);
     var favFood = ['tacos','steak','spaghetti','chilli', 'pho'];
     for (var i = 0; i < favFood.length; i++){
       if (guessFood === favFood[i]){
         alert('Awesome you guessed one!');
-        console.log('Awesome ' + guessFood + ' is one of my favorites');
+        //console.log('Awesome ' + guessFood + ' is one of my favorites');
         userPoints++;
         numOfGuess3 = 5;
         break;
@@ -127,7 +131,7 @@ function questionEight() {
 }
 questionEight();
 
-console.log(''+ user + ' has ' + userPoints + ' point(s).');
+//console.log(''+ user + ' has ' + userPoints + ' point(s).');
 
 
 alert('That\'s it! Lets see how you did: ' + user + ', You\'ve earned ' + userPoints + ' point(s) out of 8 total points.');
